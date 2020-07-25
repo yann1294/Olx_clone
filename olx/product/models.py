82e8639151302e51e0f7980920fa25462bbeac28
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-07-25 18:41:06 +0530

    added Brand and Product Images Models

--- olx/product/models.py
+++ olx/product/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=5)
     created = models.DateTimeField(default=timezone.now)
 
@@ -24,10 +25,21 @@ class Product(models.Model):
         return self.name
 
 
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.product.name
+
+    class Meta:
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'
+
 class Category(models.Model):
     ## for product category
     category_name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='category/', blank=True, null=True)
 
     class Meta:
         verbose_name = 'category'
@@ -35,3 +47,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+
+class Brand(models.Model):
+    ## for product category
+    brand_name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'brand'
+        verbose_name_plural = 'brands'
+
+    def __str__(self):
+        return self.brand_name

5bd4f03d68ccbf9c3e95ef89e44ccc1cd79b3b52
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-07-25 16:07:30 +0530

    added product app model

--- olx/product/models.py
+++ olx/product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Product(models.Model):
@@ -11,10 +12,11 @@ class Product(models.Model):
     ## contain all the products information
 
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=100, choices=CONDITION_TYPE)
     price = models.DecimalField(max_digits=10, decimal_places=5)
-    created = models.DateTimeField()
+    created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name

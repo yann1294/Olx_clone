4cba33e3c0f8e56327d48483cef787f1b85219af
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-08-11 00:58:10 +0530

    Added Products App Views

--- olx/product/views.py
+++ olx/product/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
 
 def productlist(request):
-    pass
+    productlist = Product.objects.all()
+
+    template = 'Product/product_list.html'
+
+    context = {'product_list': productlist}
+
+    return render(request, template, context)
 
 
 def productdetail(request, id):
     print(id)
+    productdetail = Product.objects.get(id=id)
+    template = 'Product/product_detail.html'
+    context = {'product_detail': productdetail}
+    return render(request, template, context)

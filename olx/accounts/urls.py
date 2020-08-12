9caf820cd9929c69172e15e24611664518a424d7
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-08-12 20:58:11 +0530

    Added User Accounts 2

--- olx/accounts/urls.py
+++ olx/accounts/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
 
 # from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', login, {'template_name': 'registration/login.html'}, name='name'),
-    # path('<slug:category_slug>', views.productlist, name='product_list_category'),
-    # path('detail/<slug:product_slug>', views.productdetail, name='product_detail')
+    path('login/', auth_views.LoginView.as_view(),  name='login'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(),  name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),  name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),  name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),  name='password_reset_done'),
+
+
 ]
--- olx/olx/urls.py
+++ olx/olx/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('product.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
+    # path('accounts/', include('accounts.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

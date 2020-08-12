782263e1028b67e071a2de5ef8d42dc4078dad3c
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-08-12 06:04:28 +0530

    Activated Categories In Template

--- olx/product/views.py
+++ olx/product/views.py
@@ -6,19 +6,24 @@ from django.db.models import Count
 
 # Create your views here.
 
-def productlist(request):
+def productlist(request, category_slug=None):
+    category = None
+
     productlist = Product.objects.all()
 
     categoryList = Category.objects.annotate(total_products=Count('product'))
 
-    template = 'Product/product_list.html'
+    if category_slug:
+        category = Category.objects.get(slug=category_slug)
+        productlist = productlist.filter(category=category)
 
     paginator = Paginator(productlist, 1)
 
     page_number = request.GET.get('page')
     productlist = paginator.get_page(page_number)
+    template = 'Product/product_list.html'
 
-    context = {'product_list': productlist, 'category_list': categoryList}
+    context = {'product_list': productlist, 'category_list': categoryList, 'category': category}
 
     return render(request, template, context)
 
@@ -28,5 +33,5 @@ def productdetail(request, product_slug):
     productdetail = Product.objects.get(slug=product_slug)
     productimages = ProductImages.objects.filter(product=productdetail)
     template = 'Product/product_detail.html'
-    context = {'product_detail': productdetail, 'product_images' : productimages}
+    context = {'product_detail': productdetail, 'product_images': productimages}
     return render(request, template, context)

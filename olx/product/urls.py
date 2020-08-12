6c24ae34dae05b15c1a6dc3fd547b2d90692cf46
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-08-12 06:04:28 +0530

    Activated Categories In Template

--- olx/product/urls.py
+++ olx/product/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.productlist, name='product_list'),
+    path('<slug:category_slug>', views.productlist, name='product_list_category'),
     path('<slug:product_slug>', views.productdetail, name='product_detail')
 ]

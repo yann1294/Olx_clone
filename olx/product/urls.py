0c4b399ca8bd70c7cfb2447059709d9b0261b4ea
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-08-12 18:27:01 +0530

    Fixing Views and Urls

--- olx/product/urls.py
+++ olx/product/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.productlist, name='product_list'),
     path('<slug:category_slug>', views.productlist, name='product_list_category'),
-    path('<slug:product_slug>', views.productdetail, name='product_detail')
+    path('detail/<slug:product_slug>', views.productdetail, name='product_detail')
 ]
